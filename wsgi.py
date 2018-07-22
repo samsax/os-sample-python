59e999003612b79082f79d909f69a662191a8194
samsax <samuelsaxofon@gmail.com>
2018-07-22 14:26:48 -0500

    article example

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, url_for
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "First Attemp!"
+
+@application.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+@application.route('/articles/<articleid>')
+def api_article(articleid):
+    return 'You are reading ' + articleid
 
 if __name__ == "__main__":
     application.run()
